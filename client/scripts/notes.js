185768045d8d537f27812911b8ed8b1ff664714d
justinbeharry <jsbeharr@umich.edu>
2021-11-26 12:17:42 -0500

    Add functionality to edit note and cancel button
    *Edit note should enable text box for editing
    *Cancel should reset note to the last input

--- client/scripts/notes.js
+++ client/scripts/notes.js
@@ -1,30 +1,72 @@
+const editClick = (index) => {
+	return function () {
+		// enable text box
+		document.querySelector(`input[name="${index}"]`).disabled = false
+		// enable confirm and cancel button
+		document.querySelector(`#confirm${index}`).className = "edit";
+		document.querySelector(`#cancel${index}`).className = "delete";
+		// disable edit button
+		document.querySelector(`#edit${index}`).className = "unavailable";
+	}
+}
+
+const cancelClick = (note, index) => {
+	return function () {
+		// disable  text box and set original note
+		document.querySelector(`input[name="${index}"]`).disabled = true;
+		document.querySelector(`input[name="${index}"]`).value = note;
+		// disable confirm and cancel button
+		document.querySelector(`#confirm${index}`).className = "unavailable";
+		document.querySelector(`#cancel${index}`).className = "unavailable";
+		// re enable edit button
+		document.querySelector(`#edit${index}`).className = "edit";
+	}
+}
+
 const usernameClick = (name) => {
 	return function () {
 		fetch(`http://localhost:3000/api/note/${name}`)
 			.then(res => res.json())
 			.then(data => {
+				// Get and Initialize User Notes section
 				document.querySelector("#user-notes-title").innerHTML = `${name} Notes`;
 				const notesList = document.querySelector("#user-notes-input");
 				notesList.innerHTML = ""
 
+				// For every note setup an input box, buttons, and setup click listeners
 				const notes = data.notes;
 				notes.forEach((note, index) => {
 					const input = document.createElement("input");
 					input.type = "text";
-					input.name = note.index;
-					input.placeholder = note;
+					input.name = index;
+					input.value = note;
 					input.disabled = true;
 
 					const editButton = document.createElement("button")
-					editButton.id = "edit";
+					editButton.id = `edit${index}`;
+					editButton.className = "edit";
+					editButton.onclick = editClick(index);
 					editButton.appendChild(document.createTextNode("Edit Note"));
 
+					const confirmButton = document.createElement("button")
+					confirmButton.id = `confirm${index}`;
+					confirmButton.className = "unavailable";
+					confirmButton.appendChild(document.createTextNode("Confirm Change"));
+
+					const cancelButton = document.createElement("button")
+					cancelButton.id = `cancel${index}`;
+					cancelButton.className = "unavailable";
+					cancelButton.onclick = cancelClick(note, index);
+					cancelButton.appendChild(document.createTextNode("Cancel"));
+
 					const deleteButton = document.createElement("button")
-					deleteButton.id = "delete";
+					deleteButton.className = "delete";
 					deleteButton.appendChild(document.createTextNode("Delete Note"));
 
 					notesList.appendChild(input);
 					notesList.appendChild(editButton);
+					notesList.appendChild(confirmButton);
+					notesList.appendChild(cancelButton)
 					notesList.appendChild(deleteButton);
 				});
 			})
